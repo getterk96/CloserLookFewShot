17c5aa567d0e8251cc6ca68c0e7edeed1d3bc67a
getterk96 <getterk@163.com>
2019-08-06 09:40:03 +0800

    update

--- methods/__init__.py
+++ methods/__init__.py
@@ -1,8 +1,7 @@
 from . import meta_template
 from . import baselinetrain
-from . import baselinefinetune 
+from . import baselinefinetune
 from . import matchingnet
 from . import protonet
 from . import relationnet
 from . import maml
-
